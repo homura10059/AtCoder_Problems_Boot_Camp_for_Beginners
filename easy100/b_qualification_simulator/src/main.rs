adf0d250867168970dd533f32cc539bcc20edc56
homura <preasper0+github@gmail.com>
2023-09-28 20:02:24 +0900

    update

--- easy100/b_qualification_simulator/src/main.rs
+++ easy100/b_qualification_simulator/src/main.rs
@@ -1,20 +1,20 @@
 use proconio::input;
 
-fn calc(a: i32, b: i32, s: &Vec<char>) -> Vec<bool> {
-    let all_students = s.iter().map(|c| State::from(*c)).collect::<Vec<State>>();
-
+fn calc(a: i32, b: i32, s: String) -> Vec<bool> {
     let mut results: Vec<bool> = vec![];
     let total = (a + b) as usize;
     let mut b_count = 1;
+    let mut true_count = 0;
 
-    for student in all_students {
-        let true_count = results.iter().filter(|r| **r).count();
-        if student == State::A && true_count < total {
+    for student in s.chars() {
+        if student == 'a' && true_count < total {
             results.push(true);
+            true_count += 1;
             continue;
         }
-        if student == State::B && true_count < total && b_count <= b {
+        if student == 'b' && true_count < total && b_count <= b {
             results.push(true);
+            true_count += 1;
             b_count += 1;
             continue;
         }
@@ -24,32 +24,13 @@ fn calc(a: i32, b: i32, s: &Vec<char>) -> Vec<bool> {
     results
 }
 
-#[derive(PartialEq, Copy, Clone)]
-enum State {
-    A,
-    B,
-    C,
-}
-
-impl From<char> for State {
-    fn from(item: char) -> Self {
-        if item == 'a' {
-            State::A
-        } else if item == 'b' {
-            State::B
-        } else {
-            State::C
-        }
-    }
-}
-
 fn main() {
     input! {
         (n, a, b): (i32, i32, i32),
         s: String
     }
 
-    let results = calc(a, b, &s);
+    let results = calc(a, b, s);
     for result in results {
         let r = if result { "Yes" } else { "No" };
         println!("{}", r)
@@ -62,11 +43,7 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let actual = calc(
-            2,
-            3,
-            &vec!['a', 'b', 'c', 'c', 'a', 'b', 'a', 'a', 'b', 'b'],
-        );
+        let actual = calc(2, 3, "abccabaabb".to_string());
         assert_eq!(
             actual,
             vec![true, true, false, false, true, true, true, false, false, false]
