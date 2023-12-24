c109c657346aa9139b0061454d4e88fbb5585fda
homura <preasper0+github@gmail.com>
2023-12-24 20:50:36 +0900

    add

--- easy100/c-exception-handling/src/main.rs
+++ easy100/c-exception-handling/src/main.rs
@@ -2,21 +2,25 @@
 
 use proconio::input;
 
-// ABC086C - Traveling
-// https://atcoder.jp/contests/abs/tasks/arc089_a
-
 fn main() {
     input! {
         n: usize,
-        mut plan: [(i32, i32, i32); n],  // Vec<(i32, i32, i32)>
+        a: [usize; n],
     }
-    plan.insert(0, (0, 0, 0));
-    let yes = plan.windows(2).all(|w| {
-        let (t0, x0, y0) = w[0];
-        let (t1, x1, y1) = w[1];
-        let time = t1 - t0;
-        let dist = (x1 - x0).abs() + (y1 - y0).abs();
-        dist <= time && time % 2 == dist % 2
+    let mut nums = a.clone();
+    nums.sort();
+    let (next_max, max) = (nums[n - 2], nums[n - 1]);
+    let max_count = a.iter().filter(|&x| *x == max).count();
+
+    a.iter().for_each(|x| {
+        if *x == max {
+            if max_count == 1 {
+                println!("{}", next_max);
+            } else {
+                println!("{}", max);
+            }
+        } else {
+            println!("{}", max);
+        }
     });
-    println!("{}", if yes { "Yes" } else { "No" });
 }
--- easy100/c-exception-handling/tests/sample_inputs.rs
+++ easy100/c-exception-handling/tests/sample_inputs.rs
@@ -7,13 +7,22 @@ fn sample1() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
-        .output_with_stdin(r#"2
-3 1 2
-6 1 1
-"#)
+        .output_with_stdin(
+            r#"3
+1
+4
+3
+"#,
+        )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "Yes\n");
+    assert_eq!(
+        output.stdout_str(),
+        r#"4
+3
+4
+"#
+    );
     assert!(output.stderr_str().is_empty());
 }
 
@@ -22,27 +31,19 @@ fn sample2() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
-        .output_with_stdin(r#"1
-2 100 100
-"#)
+        .output_with_stdin(
+            r#"2
+5
+5
+"#,
+        )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "No\n");
+    assert_eq!(
+        output.stdout_str(),
+        r#"5
+5
+"#
+    );
     assert!(output.stderr_str().is_empty());
 }
-
-#[test]
-fn sample3() {
-    let testdir = TestDir::new(BIN, "");
-    let output = testdir
-        .cmd()
-        .output_with_stdin(r#"2
-5 1 1
-100 1 1
-"#)
-        .tee_output()
-        .expect_success();
-    assert_eq!(output.stdout_str(), "No\n");
-    assert!(output.stderr_str().is_empty());
-}
-
