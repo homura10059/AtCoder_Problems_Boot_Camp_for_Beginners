0af06ceeeaa24f2edea963de3ed8b02f3b79da32
homura <preasper0+github@gmail.com>
2024-11-17 20:41:26 +0900

    add

--- easy100/a-digit-sum-2/src/main.rs
+++ easy100/a-digit-sum-2/src/main.rs
@@ -15,9 +15,10 @@ fn main() {
     let mut result = (0..n.len())
         .map(|i| {
             let num = if nums[i] == 0 { 0 } else { nums[i] - 1 };
-            let nine_sum = 9 * (n.len() - (i + 1)) as u32;
+            let after_sum = 9 * (n.len() - (i + 1)) as u32;
+            let before_sum = nums[0..i].iter().sum::<u32>();
 
-            nine_sum + num
+            before_sum + num + after_sum
         })
         .collect::<Vec<u32>>();
     result.push(nums.iter().sum());
